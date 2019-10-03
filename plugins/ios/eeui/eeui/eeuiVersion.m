aeee67baa5f278533b1362fa5d58e37bbdd5d885
kuaifan <aipaw@live.cn>
2019-10-03 15:52:05 +0800

    no message

--- plugins/ios/eeui/eeui/eeuiVersion.m
+++ plugins/ios/eeui/eeui/eeuiVersion.m
@@ -12,12 +12,12 @@
 
 + (NSString*)eeuiVersion
 {
-    return @"16";    //versionCode
+    return @"17";    //versionCode
 }
 
 + (NSString*)eeuiVersionName
 {
-    return @"1.0.16";    //versionName
+    return @"1.0.17";    //versionName
 }
 
 @end
