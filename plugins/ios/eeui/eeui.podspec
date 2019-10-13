fa6fcac759e3f87e4359896ab9dcfa08a7ddef12
kuaifan <aipaw@live.cn>
2019-10-13 21:00:48 +0800

    no message

--- plugins/ios/eeui/eeui.podspec
+++ plugins/ios/eeui/eeui.podspec
@@ -5,7 +5,7 @@ Pod::Spec.new do |s|
 
 
   s.name         = "eeui"
-  s.version      = "1.0.19"
+  s.version      = "1.0.20"
   s.summary      = "eeui plugin."
   s.description  = <<-DESC
                     eeui plugin.
--- plugins/ios/eeui/eeui/eeuiVersion.m
+++ plugins/ios/eeui/eeui/eeuiVersion.m
@@ -12,12 +12,12 @@
 
 + (NSString*)eeuiVersion
 {
-    return @"19";    //versionCode
+    return @"20";    //versionCode
 }
 
 + (NSString*)eeuiVersionName
 {
-    return @"1.0.19";    //versionName
+    return @"1.0.20";    //versionName
 }
 
 @end
