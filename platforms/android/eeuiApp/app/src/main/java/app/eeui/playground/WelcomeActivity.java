ee2aef7d0260b1830f64cd591be139c2a582cfb8
kuaifan <aipaw@live.cn>
2019-12-08 23:51:02 +0800

    no message

--- platforms/android/eeuiApp/app/src/main/java/app/eeui/playground/WelcomeActivity.java
+++ platforms/android/eeuiApp/app/src/main/java/app/eeui/playground/WelcomeActivity.java
@@ -58,9 +58,9 @@ public class WelcomeActivity extends AppCompatActivity {
             mPageBean.setCache(eeuiParse.parseLong(eeuiBase.config.getHomeParams("cache", "0")));
             mPageBean.setLoading(eeuiParse.parseBool(eeuiBase.config.getHomeParams("loading", "true")));
             mPageBean.setLoadingBackground(eeuiParse.parseBool(eeuiBase.config.getHomeParams("loadingBackground", "false")));
-            mPageBean.setSwipeBack(eeuiParse.parseBool(eeuiBase.config.getHomeParams("swipeBack", "false")));
+            mPageBean.setSwipeBack(eeuiParse.parseBool(eeuiBase.config.getHomeParams("swipeBack", "true")));
             mPageBean.setSwipeFullBack(eeuiParse.parseBool(eeuiBase.config.getHomeParams("swipeFullBack", "false")));
-            mPageBean.setSwipeColorBack(eeuiParse.parseBool(eeuiBase.config.getHomeParams("swipeColorBack", "false")));
+            mPageBean.setSwipeColorBack(eeuiParse.parseBool(eeuiBase.config.getHomeParams("swipeColorBack", "true")));
             mPageBean.setStatusBarType(eeuiBase.config.getHomeParams("statusBarType", "normal"));
             mPageBean.setStatusBarColor(eeuiBase.config.getHomeParams("statusBarColor", "#3EB4FF"));
             mPageBean.setStatusBarAlpha(eeuiParse.parseInt(eeuiBase.config.getHomeParams("statusBarAlpha", "0")));
