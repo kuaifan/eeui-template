143279db25893b8dcd6d663727d7fe4b44dcba91
kuaifan <aipaw@live.cn>
2019-12-13 23:26:40 +0800

    no message

--- eeui.config.js
+++ eeui.config.js
@@ -11,7 +11,7 @@ module.exports = {
     appKey: "3TRWyttKzxRBtmHc004sJMjjyAxOf08l",
 
     android: {
-        imageEngine: "",
+        imageEngine: "picasso",
     },
 
     wxpay: {
--- platforms/ios/eeuiApp/bundlejs/eeui/appboard/custom.js
+++ platforms/ios/eeuiApp/bundlejs/eeui/appboard/custom.js
@@ -3,7 +3,7 @@ var eeui = app.requireModule('eeui');
 Vue.mixin({
     data() {
         return {
-            resourceUrl: 'https://editor.eeui.app/compile/editor/releases/1/official/3d888453570367d6173bdc4397b52496/src/pages/',
+            resourceUrl: 'https://editor.eeui.app/compile/editor/releases/1/official/a1133cf10fe4e8490845c465b1e08d39/src/pages/',
         }
     },
 
