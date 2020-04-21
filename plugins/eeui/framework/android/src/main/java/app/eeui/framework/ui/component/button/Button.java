f9fb3484e8490ebd18746056b4ebca2f3ff0b2e7
kuaifan <aipaw@live.cn>
2020-04-21 18:04:16 +0800

    修复Android端Button组件

--- plugins/eeui/framework/android/src/main/java/app/eeui/framework/ui/component/button/Button.java
+++ plugins/eeui/framework/android/src/main/java/app/eeui/framework/ui/component/button/Button.java
@@ -2,20 +2,20 @@ package app.eeui.framework.ui.component.button;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.drawable.GradientDrawable;
 import androidx.annotation.NonNull;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.alibaba.fastjson.JSONObject;
 import com.taobao.weex.WXSDKInstance;
 import com.taobao.weex.annotation.JSMethod;
+import com.taobao.weex.dom.WXAttr;
 import com.taobao.weex.ui.action.BasicComponentData;
 import com.taobao.weex.ui.component.WXVContainer;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import app.eeui.framework.R;
@@ -38,19 +38,30 @@ public class Button extends WXVContainer<ViewGroup> implements View.OnClickListe
     private boolean isDisabled;
     private boolean isLoading;
 
-    private FrameLayout l_button;
     private IconTextView v_loading;
     private View v_unclick;
     private TextView v_text;
 
-    private int button_radius = eeuiScreenUtils.weexPx2dp(getInstance(), 8, 0);
-    private int button_backgroundColor = 0xFF3EB4FF;
-    private int button_borderWidth;
-    private int button_borderColor;
     private int text_color = 0xFFFFFFFF;
 
     public Button(WXSDKInstance instance, WXVContainer parent, BasicComponentData basicComponentData) {
         super(instance, parent, basicComponentData);
+        //
+        String modelVal = String.valueOf(getComponentValue("model"));
+        Map<String, Object> addStyle = new HashMap<>();
+        if (getComponentValue("borderRadius") == null) {
+            addStyle.put("borderRadius", "8px");
+        }
+        if (getComponentValue("borderWidth") == null) {
+            addStyle.put("borderWidth", "0px");
+        }
+        if (getComponentValue("backgroundColor") == null) {
+            addStyle.put("backgroundColor", modelToColor(modelVal));
+        }
+        if (modelVal.toLowerCase().equals("white")) {
+            text_color = 0xFF000000;
+        }
+        basicComponentData.addStyle(addStyle, true);
     }
 
     @Override
@@ -66,7 +77,6 @@ public class Button extends WXVContainer<ViewGroup> implements View.OnClickListe
     }
 
     private void initPagerView() {
-        l_button = mView.findViewById(R.id.l_button);
         v_loading = mView.findViewById(R.id.v_loading);
         v_unclick = mView.findViewById(R.id.v_unclick);
         v_text = mView.findViewById(R.id.v_text);
@@ -111,22 +121,6 @@ public class Button extends WXVContainer<ViewGroup> implements View.OnClickListe
                 setTextSize(val);
                 return true;
 
-            case "backgroundColor":
-                setBackgroundColor(val);
-                return true;
-
-            case "borderRadius":
-                setRadius(val);
-                return true;
-
-            case "borderWidth":
-                setBorder(val, null);
-                return true;
-
-            case "borderColor":
-                setBorder(null, val);
-                return true;
-
             case "disabled":
                 setDisabled(val);
                 return true;
@@ -135,36 +129,75 @@ public class Button extends WXVContainer<ViewGroup> implements View.OnClickListe
                 setLoading(val);
                 return true;
 
-            case "model":
-                setModel(val);
-                return true;
-
             default:
                 return false;
         }
     }
 
+    private Object getComponentValue(String name) {
+        Object val = getBasicComponentData().getStyles().get(name);
+        if (val != null) {
+            return val;
+        }
+        WXAttr mWXAttr = getBasicComponentData().getAttrs();
+        val = mWXAttr.get(name);
+        if (val != null) {
+            return val;
+        }
+        JSONObject json = eeuiJson.parseObject(eeuiParse.parseStr(mWXAttr.get("eeui"), ""));
+        return json.get(name);
+    }
+
+    private String modelToColor(String model) {
+        String button_backgroundColor = "#3EB4FF";
+        switch (model.toLowerCase()) {
+            case "red":
+                button_backgroundColor = "#f44336";
+                break;
+
+            case "green":
+                button_backgroundColor = "#4caf50";
+                break;
+
+            case "blue":
+                button_backgroundColor = "#2196f3";
+                break;
+
+            case "pink":
+                button_backgroundColor = "#e91e63";
+                break;
+
+            case "yellow":
+                button_backgroundColor = "#ffeb3b";
+                break;
+
+            case "orange":
+                button_backgroundColor = "#ff9800";
+                break;
+
+            case "gray":
+                button_backgroundColor = "#9e9e9e";
+                break;
+
+            case "black":
+                button_backgroundColor = "#000000";
+                break;
+
+            case "white":
+                button_backgroundColor = "#ffffff";
+                break;
+        }
+        return button_backgroundColor;
+    }
+
     private void updateStyle() {
-        int buttonBackgroundColor = button_backgroundColor;
-        int buttonBorderColor = button_borderColor;
         int textColor =  text_color;
         if (isDisabled) {
-            buttonBackgroundColor = eeuiParse.parseColor("#1E000000");
-            buttonBorderColor = eeuiParse.parseColor("#20000000");
             textColor = eeuiParse.parseColor("#ffffff");
         }
         v_loading.setVisibility(isLoading ? View.VISIBLE : View.GONE);
         v_unclick.setVisibility(isLoading || isDisabled ? View.VISIBLE : View.GONE);
         //
-        GradientDrawable gd = new GradientDrawable();
-        gd.setColor(buttonBackgroundColor);
-        if (button_radius > 0) {
-            gd.setCornerRadius(button_radius);
-        }
-        if (button_borderWidth > 0) {
-            gd.setStroke(button_borderWidth, buttonBorderColor);
-        }
-        l_button.setBackground(gd);
         v_text.setTextColor(textColor);
     }
 
@@ -198,94 +231,6 @@ public class Button extends WXVContainer<ViewGroup> implements View.OnClickListe
     @JSMethod
     public void setTextSize(Object var) {
         v_text.setTextSize(TypedValue.COMPLEX_UNIT_PX, eeuiScreenUtils.weexPx2dp(getInstance(), var, 24));
-    }
-
-    /**
-     * 设置风格
-     * @param var
-     */
-    @JSMethod
-    public void setModel(Object var) {
-        switch (eeuiParse.parseStr(var).toLowerCase()) {
-            case "red":
-                button_backgroundColor = eeuiParse.parseColor("#f44336");
-                break;
-
-            case "green":
-                button_backgroundColor = eeuiParse.parseColor("#4caf50");
-                break;
-
-            case "blue":
-                button_backgroundColor = eeuiParse.parseColor("#2196f3");
-                break;
-
-            case "pink":
-                button_backgroundColor = eeuiParse.parseColor("#e91e63");
-                break;
-
-            case "yellow":
-                button_backgroundColor = eeuiParse.parseColor("#ffeb3b");
-                break;
-
-            case "orange":
-                button_backgroundColor = eeuiParse.parseColor("#ff9800");
-                break;
-
-            case "gray":
-                button_backgroundColor = eeuiParse.parseColor("#9e9e9e");
-                break;
-
-            case "black":
-                button_backgroundColor = eeuiParse.parseColor("#000000");
-                break;
-
-            case "white":
-                button_backgroundColor = eeuiParse.parseColor("#ffffff");
-                break;
-        }
-        if (eeuiParse.parseStr(var).toLowerCase().equals("white")) {
-            setTextColor("#000000");
-        }else{
-            setTextColor("#ffffff");
-        }
-        updateStyle();
-    }
-
-    /**
-     * 设置圆角
-     * @param var
-     */
-    @JSMethod
-    public void setRadius(Object var) {
-        button_radius = eeuiScreenUtils.weexPx2dp(getInstance(), var, 0);
-        updateStyle();
-    }
-
-
-    /**
-     * 设置背景颜色
-     * @param var
-     */
-    @JSMethod
-    public void setBackgroundColor(Object var) {
-        button_backgroundColor = eeuiParse.parseColor(eeuiParse.parseStr(var));
-        updateStyle();
-    }
-
-    /**
-     * 设置边框
-     * @param width
-     * @param color
-     */
-    @JSMethod
-    public void setBorder(Object width, Object color) {
-        if (width != null) {
-            button_borderWidth = eeuiScreenUtils.weexPx2dp(getInstance(), width, 0);
-        }
-        if (color != null) {
-            button_borderColor = eeuiParse.parseColor(eeuiParse.parseStr(color));
-        }
-        updateStyle();
     }
 
     /**
