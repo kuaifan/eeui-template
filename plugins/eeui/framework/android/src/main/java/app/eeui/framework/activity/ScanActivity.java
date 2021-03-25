39191b256a83894ad8a307f468d416e31dbe2f9c
yujianpeng <545349595@qq.com>
2021-03-25 17:33:32 +0800

    modify parse Photo code

--- plugins/eeui/framework/android/src/main/java/app/eeui/framework/activity/ScanActivity.java
+++ plugins/eeui/framework/android/src/main/java/app/eeui/framework/activity/ScanActivity.java
@@ -3,9 +3,13 @@ package app.eeui.framework.activity;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
@@ -16,11 +20,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
 import com.google.zxing.DecodeHintType;
+import com.google.zxing.FormatException;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
 import com.taobao.weex.bridge.JSCallback;
 
+import java.io.FileNotFoundException;
+
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
@@ -201,7 +215,7 @@ public class ScanActivity extends AppCompatActivity implements OnCaptureCallback
         new Thread(() -> {
             Result result = parseCode(path);
             runOnUiThread(() -> {
-                if (mJSCallback != null) {
+                if (mJSCallback != null && result != null) {
                     Map<String, Object> retData = new HashMap<>();
                     retData.put("status", "success");
                     retData.put("source", "photo");
@@ -223,6 +237,7 @@ public class ScanActivity extends AppCompatActivity implements OnCaptureCallback
      * @return
      */
     public static Result parseCode(String bitmapPath){
+        /*
         Map<DecodeHintType,Object> hints = new HashMap<>();
         //添加可以解析的编码类型
         Vector<BarcodeFormat> decodeFormats = new Vector<>();
@@ -235,7 +250,98 @@ public class ScanActivity extends AppCompatActivity implements OnCaptureCallback
         hints.put(DecodeHintType.TRY_HARDER,Boolean.TRUE);
         hints.put(DecodeHintType.POSSIBLE_FORMATS, decodeFormats);
         return CodeUtils.parseCodeResult(bitmapPath,hints);
+         */
+
+        Hashtable<DecodeHintType, String> hints = new Hashtable<>();
+        hints.put(DecodeHintType.CHARACTER_SET, "UTF8"); //设置二维码内容的编码
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = false; // 获取缩略图
+        Bitmap scanBitmap = BitmapFactory.decodeFile(bitmapPath, options);
+        RGBLuminanceSource rgbLuminanceSource = new RGBLuminanceSource(scanBitmap);
+        BinaryBitmap bitmap1 = new BinaryBitmap(new HybridBinarizer(rgbLuminanceSource));
+
+
+        QRCodeReader reader = new QRCodeReader();
+        try {
+            return reader.decode(bitmap1, hints);
+        }  catch (NotFoundException e) {
+                Log.e("yjp","NotFoundException");
+            }catch (ChecksumException e){
+                Log.e("yjp","ChecksumException");
+            }catch(FormatException e){
+                Log.e("yjp","FormatException");
+            }
+        return null;
     }
+
+    public static final class RGBLuminanceSource extends LuminanceSource {
+
+        private final byte[] luminances;
+
+        public RGBLuminanceSource(Bitmap bitmap) {
+            super(bitmap.getWidth(), bitmap.getHeight());
+
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+
+            int[] pixels = new int[width * height];
+            bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+
+            // In order to measure pure decoding speed, we convert the entire image
+            // to a greyscale array
+            // up front, which is the same as the Y channel of the
+            // YUVLuminanceSource in the real app.
+            luminances = new byte[width * height];
+            for (int y = 0; y < height; y++) {
+                int offset = y * width;
+                for (int x = 0; x < width; x++) {
+                    int pixel = pixels[offset + x];
+                    int r = (pixel >> 16) & 0xff;
+                    int g = (pixel >> 8) & 0xff;
+                    int b = pixel & 0xff;
+                    if (r == g && g == b) {
+                        // Image is already greyscale, so pick any channel.
+                        luminances[offset + x] = (byte) r;
+                    } else {
+                        // Calculate luminance cheaply, favoring green.
+                        luminances[offset + x] = (byte) ((r + g + g + b) >> 2);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public byte[] getRow(int y, byte[] row) {
+            if (y < 0 || y >= getHeight()) {
+                throw new IllegalArgumentException("Requested row is outside the image: " + y);
+            }
+            int width = getWidth();
+            if (row == null || row.length < width) {
+                row = new byte[width];
+            }
+
+            System.arraycopy(luminances, y * width, row, 0, width);
+            return row;
+        }
+
+        // Since this class does not support cropping, the underlying byte array
+        // already contains
+        // exactly what the caller is asking for, so give it to them without a copy.
+        @Override
+        public byte[] getMatrix() {
+            return luminances;
+        }
+
+        private  Bitmap loadBitmap(String path) throws FileNotFoundException {
+            Bitmap bitmap = BitmapFactory.decodeFile(path);
+            if (bitmap == null) {
+                throw new FileNotFoundException("Couldn't open " + path);
+            }
+            return bitmap;
+        }
+
+    }
+
 
     /**
      * 选择相册
