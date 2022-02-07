9dcac4717b3868074653b82698a43dd2f6f1b79d
fzq <617323152@qq.com>
2022-02-07 14:45:32 +0800

    编译错误修改

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,18 @@ const uglify = require("gulp-uglify");
 const babel = require("gulp-babel");
 const argv = require('minimist')(process.argv.slice(2));
 const babelOptions = require('./babel.config.js');
-
-gulp.task('appboard-dev', () => {
+const dev = () => {
     return gulp.src(argv.filePath || './common/dist/appboard/*.js')
         .pipe(babel(babelOptions))
         .pipe(gulp.dest('./common/dist/appboard'));
-});
-
-gulp.task('appboard-build', () => {
+};
+const build = () => {
     return gulp.src('./common/dist/appboard/*.js')
         .pipe(babel(babelOptions))
         .pipe(uglify())
         .pipe(gulp.dest('./common/dist/appboard'));
-});
+};
 
-gulp.task('default', gulp.parallel('appboard-dev'));
+gulp.task('appboard-dev', dev);
+gulp.task('appboard-build', build);
+gulp.task('default', dev);
